8f66c3677c0e39e267ddeb844f9ef8db881fe57d
Emily Yu <emilyyu331@gmail.com>
2020-07-16 17:37:54 -0700

    sharescreen kinda works but weird lookin

--- src/components/routes/Room.js
+++ src/components/routes/Room.js
@@ -1,33 +1,24 @@
-// Video-chat logic
-
 import React, { useRef, useEffect } from "react";
 import io from "socket.io-client";
 import './styles.css'
-  
+
 const Room = (props) => {
-    // references
     const userVideo = useRef();
     const partnerVideo = useRef();
     const peerRef = useRef();
     const socketRef = useRef();
     const otherUser = useRef();
     const userStream = useRef();
+    const senders = useRef([]);
  
     useEffect(() => {
-        // ask browser for user access to video and audio
-        // Promise resolves --> returns user's stream
         navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
-            // allows us to display the actual video
             userVideo.current.srcObject = stream;
             userStream.current = stream;
 
-            // connect to socket server
             socketRef.current = io.connect("/");
-            // emit event to the server
-            // props.match.params.roomID --> pulls the ID out of the URL
             socketRef.current.emit("join room", props.match.params.roomID);
 
-            // fires depending on another user present
             socketRef.current.on('other user', userID => {
                 callUser(userID);
                 otherUser.current = userID;
@@ -46,16 +37,11 @@ const Room = (props) => {
 
     }, []);
 
-    
     function callUser(userID) {
         peerRef.current = createPeer(userID);
-        // go to our stream, get all the "tracks" which are the tracks we called earlier (audio and video)
-        // then attach peer stream to our stream
-        // give peer access to our stream
-        userStream.current.getTracks().forEach(track => peerRef.current.addTrack(track, userStream.current));
+        userStream.current.getTracks().forEach(track => senders.current.push(peerRef.current.addTrack(track, userStream.current)));
     }
 
-    // Build a webrtc peer object 
     function createPeer(userID) {
         const peer = new RTCPeerConnection({
             iceServers: [
@@ -71,42 +57,35 @@ const Room = (props) => {
         });
 
         peer.onicecandidate = handleICECandidateEvent;
-        // recieving a remote peer (sending us their stream)
         peer.ontrack = handleTrackEvent;
-        // initiate call, offer created ... 
         peer.onnegotiationneeded = () => handleNegotiationNeededEvent(userID);
 
         return peer;
     }
 
     function handleNegotiationNeededEvent(userID) {
-        // returns a promise and resolve with offer object
-        // take offer and set as remote description
         peerRef.current.createOffer().then(offer => {
             return peerRef.current.setLocalDescription(offer);
         }).then(() => {
             const payload = {
-                target: userID, // your id
+                target: userID,
                 caller: socketRef.current.id,
-                sdp: peerRef.current.localDescription // offer data
+                sdp: peerRef.current.localDescription
             };
             socketRef.current.emit("offer", payload);
         }).catch(e => console.log(e));
     }
 
     function handleRecieveCall(incoming) {
-        peerRef.current = createPeer(); // not initiating call
-        // description object --> remote
+        peerRef.current = createPeer();
         const desc = new RTCSessionDescription(incoming.sdp);
         peerRef.current.setRemoteDescription(desc).then(() => {
-            // attach streams
             userStream.current.getTracks().forEach(track => peerRef.current.addTrack(track, userStream.current));
         }).then(() => {
             return peerRef.current.createAnswer();
         }).then(answer => {
             return peerRef.current.setLocalDescription(answer);
         }).then(() => {
-            // send data back to the caller
             const payload = {
                 target: incoming.caller,
                 caller: socketRef.current.id,
@@ -117,12 +96,10 @@ const Room = (props) => {
     }
 
     function handleAnswer(message) {
-        // bc handling answer --> set remote description
         const desc = new RTCSessionDescription(message.sdp);
         peerRef.current.setRemoteDescription(desc).catch(e => console.log(e));
     }
 
-    // recieves an event
     function handleICECandidateEvent(e) {
         if (e.candidate) {
             const payload = {
@@ -144,10 +121,21 @@ const Room = (props) => {
         partnerVideo.current.srcObject = e.streams[0];
     };
 
+    function shareScreen() {
+        navigator.mediaDevices.getDisplayMedia({ cursor: true }).then(stream => {
+            const screenTrack = stream.getTracks()[0];
+            senders.current.find(sender => sender.track.kind === 'video').replaceTrack(screenTrack);
+            screenTrack.onended = function() {
+                senders.current.find(sender => sender.track.kind === "video").replaceTrack(userStream.current.getTracks()[1]);
+            }
+        })
+    }
+
     return (
         <div>
-            <video autoPlay ref={userVideo} />
-            <video autoPlay ref={partnerVideo} />
+            <video controls style={{height: 500, width: 500}} autoPlay ref={userVideo} />
+            <video controls style={{height: 500, width: 500}} autoPlay ref={partnerVideo} />
+            <button onClick={shareScreen}>Share screen</button>
         </div>
     );
 };

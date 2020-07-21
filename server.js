311c3b3cef982ae7fab3e39bafae83767ef8493c
Emily Yu <emilyyu331@gmail.com>
2020-07-21 04:54:30 -0700

    new message chat functionality and also added comments

--- server.js
+++ server.js
@@ -1,80 +1,48 @@
-// SERVER-SIDE
-
-// imports
+//require('dotenv').config(); 
 const express = require("express");
 const http = require("http");
-// app object
 const app = express();
-// create new server
 const server = http.createServer(app);
 const socket = require("socket.io");
-// instance of io
 const io = socket(server);
 
-// rooms collection
-var rooms = {};
+const rooms = {};
 
-// when connection occurs
+//individual socket connection for a user that just connected
 io.on("connection", socket => {
-    // attach an event listener
-    // pull roomID out of the URL and send to server
-    // if room exists --> take socket id (new socket) and add it
-    // else create a new room object
     socket.on("join room", roomID => {
         if (rooms[roomID]) {
             rooms[roomID].push(socket.id);
         } else {
             rooms[roomID] = [socket.id];
         }
-        // find the other user, check to see if someone is already
-        // in the room 
-        // .find --> is there an ID that is not my own 
-        // Find and GET other id's
         const otherUser = rooms[roomID].find(id => id !== socket.id);
         if (otherUser) {
-            // emit event back
             socket.emit("other user", otherUser);
-            // emit to other user that someone has joined
-            // create the "handshake"
             socket.to(otherUser).emit("user joined", socket.id);
         }
     });
 
     socket.on("offer", payload => {
-        // send an event to the target (socket id)
-        // payload includes: (1) who you are as the caller or id and (2) offer 'object'
         io.to(payload.target).emit("offer", payload);
     });
 
     socket.on("answer", payload => {
-        // recieved the offer, answer sent back to orginal peer calling
         io.to(payload.target).emit("answer", payload);
     });
 
-    // Attempting connection --> firewalls, ip address connection, etc
-    // ice-servers i.e. stunt servers, an ice canidate allows for a pair
-    // to agree on a proper connection
-    // This event is used by both peers, back and forth until they make
-    // a "handshake"
     socket.on("ice-candidate", incoming => {
         io.to(incoming.target).emit("ice-candidate", incoming.candidate);
     });
 
-    // on disconnect
-    socket.on('disconnecting', () => {
-        // rooms associated with socket
-        const curr_rooms = Object.keys(socket.rooms);
-
-        for (const roomID in curr_rooms) {
-            // tell all rooms associated with client that they disconnected
-            io.to(roomID).emit({"disconnected": socket.id})
-            // remove socket from room in master rooms list, if room still exists
-            if (rooms[roomID]) {
-                var ind = rooms[roomID].indexOf(socket.id);
-                rooms[roomID].splice(ind, 1);
-            }
-        }
+    //sends to the client that this is your id
+    socket.emit("your id", socket.id);
+    //object that contains the message text and id of who is the sender
+    socket.on("send message", body => {
+        //send the event to all clients connected
+        io.emit("message", body)
     })
-});
+}); 
+
 
 server.listen(8000, () => console.log('server is running on port 8000'));
--- src/components/routes/Room.js
+++ src/components/routes/Room.js
@@ -1,17 +1,121 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState, useEffect, useRef } from 'react';
+import styled from "styled-components";
 import io from "socket.io-client";
-import './styles.css'
+
+//style components for the message chat box
+const Page = styled.div`
+  display: flex;
+  height: 200vh;
+  width: 100%;
+  align-items: center;
+  background-color: #46516e;
+  flex-direction: column;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 500px;
+  max-height: 500px;
+  overflow: auto;
+  width: 400px;
+  border: 1px solid lightgray;
+  border-radius: 10px;
+  padding-bottom: 10px;
+  margin-top: 25px;
+`;
+
+const TextArea = styled.textarea`
+  width: 98%;
+  height: 100px;
+  border-radius: 10px;
+  margin-top: 10px;
+  padding-left: 10px;
+  padding-top: 10px;
+  font-size: 17px;
+  background-color: transparent;
+  border: 1px solid lightgray;
+  outline: none;
+  color: lightgray;
+  letter-spacing: 1px;
+  line-height: 20px;
+  ::placeholder {
+    color: lightgray;
+  }
+`;
+
+const Button = styled.button`
+  background-color: pink;
+  width: 100%;
+  border: none;
+  height: 50px;
+  border-radius: 10px;
+  color: #46516e;
+  font-size: 17px;
+`;
+
+const Form = styled.form`
+  width: 400px;
+`;
+
+const MyRow = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 10px;
+`;
+
+const MyMessage = styled.div`
+  width: 45%;
+  background-color: pink;
+  color: #46516e;
+  padding: 10px;
+  margin-right: 5px;
+  text-align: center;
+  border-top-right-radius: 10%;
+  border-bottom-right-radius: 10%;
+`;
+
+const PartnerRow = styled(MyRow)`
+  justify-content: flex-start;
+`;
+
+const PartnerMessage = styled.div`
+  width: 45%;
+  background-color: transparent;
+  color: lightgray;
+  border: 1px solid lightgray;
+  padding: 10px;
+  margin-left: 5px;
+  text-align: center;
+  border-top-left-radius: 10%;
+  border-bottom-left-radius: 10%;
+`;
 
 const Room = (props) => {
+    /*---------------------VIDEO CHAT CONSTANTS----------------------------*/
     const userVideo = useRef();
     const partnerVideo = useRef();
     const peerRef = useRef();
     const socketRef = useRef();
     const otherUser = useRef();
     const userStream = useRef();
+
+    /*---------------------SHARESCREEN CONSTANTS----------------------------*/
     const senders = useRef([]);
- 
+
+    /*---------------------MESSAGE CHAT FUNCTIONS----------------------------*/
+    //keep track of your id
+    const [yourID, setYourID] = useState();
+    //array of messages
+    const [messages, setMessages] = useState([]);
+    //individual message tied to the message box
+    const [message, setMessage] = useState("");
+
+  
     useEffect(() => {
+
+        /*---------------------VIDEO CHAT----------------------------*/
         navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
             userVideo.current.srcObject = stream;
             userStream.current = stream;
@@ -33,12 +137,52 @@ const Room = (props) => {
             socketRef.current.on("answer", handleAnswer);
 
             socketRef.current.on("ice-candidate", handleNewICECandidateMsg);
+
+            /*---------------------MSG CHAT----------------------------*/
+            //listen to the event for when you are connecting, in return the server sends to the client your own id,
+            //so the server can keep track of who you are
+            socketRef.current.on("your id", id => {
+              setYourID(id);
+            })
+      
+            //client is sending an event back to the client, server responds with the message event
+            //sends in the message body
+            socketRef.current.on("message", (message) => {
+              console.log("here");
+              receivedMessage(message);
+            })
         });
 
     }, []);
 
+/*---------------------MSG CHAT FUNCTIONS----------------------------*/
+    //receives a message oject and appends it to state
+    function receivedMessage(message) {
+      setMessages(oldMsgs => [...oldMsgs, message]);
+    }
+
+    function sendMessage(e) {
+      e.preventDefault();
+      //your actual id and renders your message
+      const messageObject = {
+        body: message,
+        id: yourID,
+      };
+      setMessage("");
+      //send message object down to server
+      socketRef.current.emit("send message", messageObject);
+    }
+
+    function handleChange(e) {
+      setMessage(e.target.value);
+    }
+
+
+/*---------------------VIDEO CHAT FUNCTIONS----------------------------*/
     function callUser(userID) {
-        peerRef.current = createPeer(userID);
+        peerRef.current = createPeer(userID); 
+        //userStream represents the stream that contains your audio and video tracks
+        //take the tracks and put it into the senders array
         userStream.current.getTracks().forEach(track => senders.current.push(peerRef.current.addTrack(track, userStream.current)));
     }
 
@@ -121,22 +265,60 @@ const Room = (props) => {
         partnerVideo.current.srcObject = e.streams[0];
     };
 
+    /*---------------------SHARESCREEN FUNCTIONS----------------------------*/
     function shareScreen() {
+      //get displayMedia and also track the cursor
         navigator.mediaDevices.getDisplayMedia({ cursor: true }).then(stream => {
             const screenTrack = stream.getTracks()[0];
+            //do a find over the current array of senders and find the one with a track of type video, and replace that one with the
+            //current screenTrack
             senders.current.find(sender => sender.track.kind === 'video').replaceTrack(screenTrack);
+            //whenever screenTrack is no longer being used, this onended event is raised
             screenTrack.onended = function() {
+                //basically replaces the screen with your face once you stop screensharing
                 senders.current.find(sender => sender.track.kind === "video").replaceTrack(userStream.current.getTracks()[1]);
             }
         })
     }
 
     return (
-        <div>
+      /* - renders the page component, the all encompassing component
+         - the container component houses all the actual messages
+         - messages.maps iterates over the message array and uses logic to determine whether to
+          show your message or the other peer's msg 
+         - form will have an onSubmit which will call the sendMessage
+      */
+        <Page>
+          <div>
             <video controls style={{height: 500, width: 500}} autoPlay ref={userVideo} />
             <video controls style={{height: 500, width: 500}} autoPlay ref={partnerVideo} />
             <button onClick={shareScreen}>Share screen</button>
-        </div>
+          </div>
+        <Container>
+          {messages.map((message, index) => {
+            if (message.id === yourID) {
+              return (
+                <MyRow key={index}>
+                  <MyMessage>
+                    {message.body}
+                  </MyMessage>
+                </MyRow>
+              )
+            }
+              return (
+                <PartnerRow key={index}>
+                  <PartnerMessage>
+                    {message.body}
+                  </PartnerMessage>
+                </PartnerRow>
+              )
+            })}
+          </Container>
+            <Form onSubmit={sendMessage}>
+              <TextArea value={message} onChange={handleChange} placeholder="Say something..." />
+              <Button>Send</Button>
+            </Form>
+         </Page>
     );
 };
 
